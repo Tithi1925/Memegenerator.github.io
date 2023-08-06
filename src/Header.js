7eec80fe27d022ebc893d45118e86c30a50d2b40
Tithi1925 <81310263+Tithi1925@users.noreply.github.com>
2023-08-06 19:03:53 +0530

    Meme generator app

---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+export default function Header() {
+    return (
+        <header className="header">
+            <img 
+                src="https://tse1.mm.bing.net/th?id=OIP.W9CFnk1t6mzZmZUzJg34tgHaFZ&pid=Api&P=0" 
+                className="header--image"
+            />
+            <h2 className="header--title">Meme Generator</h2>
+        </header>
+    )
+}
